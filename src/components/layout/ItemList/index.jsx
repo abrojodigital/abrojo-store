f055ada990bfc101234026a354f0d9ed6bd8e700
abrojodigital <abrojodigital@gmail.com>
2023-01-31 21:01:13 -0300

    Ajustes map ItemList

--- src/components/layout/ItemList/index.jsx
+++ src/components/layout/ItemList/index.jsx
@@ -1,50 +1,52 @@
 import { ItemListContainer, Banner } from "../../";
+import { Container, Row, Col } from 'react-bootstrap';
 
-const ItemList = ({products, greeting}) => {
+const ItemList = ({ products, greeting }) => {
 
   return (
     <div className="home" id="Home">
-      <Banner imgBackGround="assets/img/cover-26.jpg" title="Colección 2023" caption="Ver ahora" heref="#season2023" greeting={greeting}/>
+      <Banner 
+        imgBackGround="assets/img/cover-26.jpg"
+        title="Colección 2023"
+        caption="Ver ahora"
+        greeting={greeting}
+      />
 
       {/* Esto a futuro debe leerse de la bbdd y mostrar paginando */}
-      <div className="section py-12">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-4">
-              <ItemListContainer product={products[0]} caption="Ver ahora" />
-            </div>
-            <div className="col-sm-4">
-              <ItemListContainer product={products[1]} caption="Ver ahora" />
-            </div>
-            <div className="col-sm-4">
-              <ItemListContainer product={products[2]} caption="Ver ahora" />
-            </div>
-          </div>
-          <div className="row justify-content-center pt-12">
-            <div className="col-12 col-md-10">
-              <div className="row align-items-center">
-                <div className="col-6">
-                  <ItemListContainer product={products[3]} caption="Ver ahora" />
-                </div>
-                <div className="col-6">
-                  <ItemListContainer product={products[4]} caption="Ver ahora" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-4">
-              <ItemListContainer product={products[5]} caption="Ver ahora" />
-            </div>
-            <div className="col-sm-4">
-              <ItemListContainer product={products[6]} caption="Ver ahora" />
-            </div>
-            <div className="col-sm-4">
-            <ItemListContainer product={products[8]} caption="Ver ahora" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container className="py-12">
+        <Row>
+          {products.map((product, index) => (
+            index < 3 ? (
+              <Col sm={4} key={index}>
+                <ItemListContainer product={product} caption="Ver ahora" />
+              </Col>
+            ) : null
+          ))}
+        </Row>
+        <Row className="justify-content-center pt-12">
+          <Col xs={12} md={10}>
+            <Row className="align-items-center">
+              {products.map((product, index) => (
+                index >= 3 && index < 5 ? (
+                  <Col xs={6} key={index}>
+                    <ItemListContainer product={product} caption="Ver ahora" />
+                  </Col>
+                ) : null
+              ))}
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          {products.map((product, index) => (
+            index >= 5 && index < 8 ? (
+              <Col sm={4} key={index}>
+                <ItemListContainer product={product} caption="Ver ahora" />
+              </Col>
+            ) : null
+          ))}
+        </Row>
+      </Container>
+
     </div>
   );
 }
