cc1f5bf5bf2f5116aa5bbfb846241625b8081474
abrojodigital <abrojodigital@gmail.com>
2023-02-13 20:26:39 -0300

    agrego firebase a index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAFJSgbepMdJxE45x-Q6dFvtZkTwRSoa6A",
+  authDomain: "abrojo-store.firebaseapp.com",
+  projectId: "abrojo-store",
+  storageBucket: "abrojo-store.appspot.com",
+  messagingSenderId: "539467183131",
+  appId: "1:539467183131:web:84ab2619fcc89bbd53b4be"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
