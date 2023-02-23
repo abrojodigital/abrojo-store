5a398ec5c770461e1f9a16250efcdf3c64303883
abrojodigital <abrojodigital@gmail.com>
2023-02-23 12:59:28 -0300

    Ajustes en App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ShoppingCartProvider } from "./context/ShoppingCartContext";
-import { Header, Footer, ModalSearch } from "./components";
+import * as React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { Header, Footer, ModalSearch } from "./components"
 import {
   FaqPage,
   GuiaTalles,
@@ -14,30 +14,30 @@ import {
   Checkout,
   ContactPage,
   ItemDetail
-} from "./pages";
+} from "./pages"
 
 export default function App() {
   return (
-    <ShoppingCartProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/products" element={<ListProducts />} />
-          <Route path="/item/:id" element={<ItemDetail />} />
-          <Route path="/products/category/:catId" element={<ListProducts />} />
-          <Route path="/faq" element={<FaqPage />} />
-          <Route path="/historia" element={<Historia />} />
-          <Route path="/talles" element={<GuiaTalles />} />
-          <Route path="/terminos" element={<TerminosCondiciones />} />
-          <Route path="/privacidad" element={<PoliticasPrivacidad />} />
-          <Route path="/modalsearch" element={<ModalSearch />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </ShoppingCartProvider>
-  );
+      <ShoppingCartProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products" element={<ListProducts />} />
+            <Route path="/item/:id" element={<ItemDetail />} />
+            <Route path="/products/category/:catId" element={<ListProducts />} />
+            <Route path="/faq" element={<FaqPage />} />
+            <Route path="/historia" element={<Historia />} />
+            <Route path="/talles" element={<GuiaTalles />} />
+            <Route path="/terminos" element={<TerminosCondiciones />} />
+            <Route path="/privacidad" element={<PoliticasPrivacidad />} />
+            <Route path="/modalsearch" element={<ModalSearch />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </ShoppingCartProvider>
+  )
 }
