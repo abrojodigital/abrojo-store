fd3835f2388c186815004d15a27585f0c727431a
abrojodigital <abrojodigital@gmail.com>
2023-02-27 15:16:25 -0300

    Ajustes en register

--- src/components/common/Register/index.jsx
+++ src/components/common/Register/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../../../context/AuthContext"
-import { Alert } from "../Alert"
+import { Alert } from "../../../components"
 import { Container, Form, Button } from "react-bootstrap"
 
 export function Register() {
